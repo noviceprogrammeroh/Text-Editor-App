fb691d1c5d5ae3aa11809ffe760ab855e0c2b29f
Hector <hperez.oh@gmail.com>
2022-11-30 21:54:43 -0500

    fixed readme file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -19,13 +19,13 @@ export const putDb = async (content) => {
     console.log('Post to the database');
 
     // Create a connection to the database database and version we want to use.
-    const contactDb = await openDB('contact', 1);
+    const contactDb = await openDB('jate', 1);
   
     // Create a new transaction and specify the database and data privileges.
-    const tx = contactDb.transaction('contact', 'readwrite');
+    const tx = contactDb.transaction('jate', 'readwrite');
   
     // Open up the desired object store.
-    const store = tx.objectStore('contact');
+    const store = tx.objectStore('jate');
   
     // Use the .add() method on the store and pass in the content.
     const request = store.put({id: 1, value: content});
@@ -41,13 +41,13 @@ export const getDb = async () => {
     console.log('GET from the database');
 
     // Create a connection to the database database and version we want to use.
-    const contactDb = await openDB('contact', 1);
+    const contactDb = await openDB('jate', 1);
   
     // Create a new transaction and specify the database and data privileges.
-    const tx = contactDb.transaction('contact', 'readonly');
+    const tx = contactDb.transaction('jate', 'readonly');
   
     // Open up the desired object store.
-    const store = tx.objectStore('contact');
+    const store = tx.objectStore('jate');
   
     // Use the .getAll() method to get all data in the database.
     const request = store.getAll();
